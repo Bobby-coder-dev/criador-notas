95d9e7b35baf986f602385430cb203a81829391d
Bobby-coder-dev <bobi77713@gmail.com>
2025-10-14 20:45:38 -0300

    Commit inicial

---
+++ notas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int opcao;
+
+	while(1){
+		printf("1 - Adicionar nota\n 2 - Listar notas\n 3 - Sair");
+		scanf("%d", &opcao);
+
+		switch(opcao){
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			default: printf("Opção Inválida");
+		}
+	}
+}
