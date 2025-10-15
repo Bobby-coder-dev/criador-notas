aef517e14808b1426f6c315e0016b99b44da5d61
bobby-coder-dev <bobi77713@gmail.com>
2025-10-15 19:27:17 -0300

    func adicionarNota

---
+++ include/notas.h
@@ -0,0 +1,6 @@
+#ifndef NOTAS_H
+#define NOTAS_H
+
+void adicionarNota(const char *nome);
+
+#endif
--- src/notas.c
+++ src/notas.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
 #include <notas.h>
+
+void adicionarNota(char *nome){
+    char txt = ".txt"
+    char nomeArquivo[100];
+
+    snprintf(nomeArquivo, sizeof(nomeArquivo), "%s%s", nome, txt);
+
+
+    FILE *arquivo;
+
+    arquivo = fopen(nomeArquivo, "w+");
+
+    if (arquivo == NULL){
+        printf("Não foi possível criar o arquivo"); 
+    }
+
+    char texto[200];
+
+    printf("Digite o conteúdo da nota:\n");
+    fgets(texto, sizeof(texto), stdin);
+
+    texto[strcspn(texto, "\n")] = '\0';
+
+    if(fputs(texto, arquivo)){
+        printf("Nota criada!, nome: %s.txt", nome);
+    } else {
+        printf("Não foi possível criar a nota");
+    }
+
+    return;
+}
